68009bcfe535b92eba865d5f303698cbb2a32cf6
bhagyashri.jakkalkar <bhagyashri.jakkalkar@ecotechservices.com>
2023-03-14 17:08:31 +0530

    End of section 4

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]    //path for this is api/users
+    public class BaseApiController : ControllerBase
+    {
+        
+    }
+}
